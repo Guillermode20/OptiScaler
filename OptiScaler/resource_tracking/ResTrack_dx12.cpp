441c5e60580c362dccdab300d302a184676c8d2e
whick <whick@example.com>
2026-08-22 22:41:09 +0100

    Log CreateSRV breadcrumbs for KCD2 async crash

--- OptiScaler/resource_tracking/ResTrack_dx12.cpp
+++ OptiScaler/resource_tracking/ResTrack_dx12.cpp
@@ -522,6 +522,25 @@ void ResTrack_Dx12::hkCreateShaderResourceView(ID3D12Device* This, ID3D12Resourc
                                                D3D12_SHADER_RESOURCE_VIEW_DESC* pDesc,
                                                D3D12_CPU_DESCRIPTOR_HANDLE DestDescriptor)
 {
+    // Crash breadcrumb: vkd3d AV'd inside buffer-SRV creation on KCD2 async.
+    {
+        static std::atomic<uint32_t> srvCount { 0 };
+        auto n = srvCount.fetch_add(1);
+        if (n < 64 || (n % 1024) == 0)
+        {
+            bool isScBuffer = false;
+            for (size_t i = 0; i < State::Instance().scBuffers.size(); i++)
+                if (State::Instance().scBuffers[i] == pResource)
+                {
+                    isScBuffer = true;
+                    break;
+                }
+            UINT dim = pDesc ? pDesc->ViewDimension : 0xFFFFFFFFu;
+            LOG_INFO("Reproj diag: CreateSRV #{}, res={:X} (scBuffer:{}), desc={:X}, dim={}", n + 1,
+                     (size_t) pResource, isScBuffer, (size_t) pDesc, (UINT) dim);
+        }
+    }
+
     // force hdr for swapchain buffer
     if (pResource != nullptr && pDesc != nullptr && Config::Instance()->ForceHDR.value_or_default())
     {
